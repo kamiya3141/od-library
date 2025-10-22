329971229b43a2974617214e9b5e28aa1a636424
kamiya3141 <kamiyatuwu@gmail.com>
2025-10-22 16:50:39 +0900

    edit some files

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,13 @@
 import { ODObject } from './components/od-object';
 import { ODRect } from './components/od-rect';
+import { ODCircle } from './components/od-circle';
 
 declare global {
 	interface HTMLElementTagNameMap {
 		'od-object': ODObject;
-		'od-rect': ODRect
+		'od-rect': ODRect;
+		'od-circle': ODCircle
 	}
 }
+
+export { ODObject, ODRect, ODCircle };
